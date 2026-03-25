76cba192d9f4c3ab05273e2027f9ea11be8e9128
twofansv <95734976+twofansv@users.noreply.github.com>
2026-03-25 22:54:36 +0800

    [+] Extended the grid boxes to 16 as per instruction.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const grid = document.querySelector('.grid');
+const container = document.querySelector('.container');
 
-grid.style.cssText = "color: blue";
+
+container.style.cssText = "display: grid; grid-template-columns: auto auto auto; background-color: dodgerblue; padding: 10px;";
+
+
+
+for (let i = 0; i < 16; i++) {
+    const grid = document.createElement('div')
+    grid.style.cssText = "background-color: #f1f1f1; border: 1px solid black; padding: 10px; font-size: 30px; text-align: center;"
+    container.appendChild(grid);
+}
+
+
+
+
 

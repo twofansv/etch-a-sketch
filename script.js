02e99d2d542683eae8a242b15a83456d2af13407
twofansv <95734976+twofansv@users.noreply.github.com>
2026-03-26 18:41:18 +0800

    [+] Completed hover paint feature.

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ for (let i = 0; i < 256; i++) {
     const grid = document.createElement('div')
     grid.style.cssText = "box-sizing: border-box; width: 6.25%; aspect-ratio: 1/1; background-color: #f1f1f1; border: 1px solid black; font-size: 30px; text-align: center;"
     container.appendChild(grid);
+
+
+    grid.addEventListener('mouseover', () => {
+        grid.style.cssText = "box-sizing: border-box; width: 6.25%; aspect-ratio: 1/1; background-color: #fa2f4a; border: 1px solid black; font-size: 30px; text-align: center;"
+  
+    });
 }
 
 

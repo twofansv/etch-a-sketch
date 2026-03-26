cd359e3b886d3d2a8fc4ef3bdeb1493e42c6b565
twofansv <95734976+twofansv@users.noreply.github.com>
2026-03-26 17:35:25 +0800

    [+] Made the grid a flexbox.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const container = document.querySelector('.container');
 
 
-container.style.cssText = "display: grid; grid-template-columns: auto auto auto; background-color: dodgerblue; padding: 10px;";
+container.style.cssText = "display: flex; flex-wrap: wrap; background-color: dodgerblue; padding: 10px;";
 
 
 
 for (let i = 0; i < 16; i++) {
     const grid = document.createElement('div')
-    grid.style.cssText = "background-color: #f1f1f1; border: 1px solid black; padding: 10px; font-size: 30px; text-align: center;"
+    grid.style.cssText = "display: flex; flex-wrap: wrap; flex-grow: 4; background-color: #f1f1f1; border: 1px solid black; padding: 10px; font-size: 30px; text-align: center;"
     container.appendChild(grid);
 }
 
